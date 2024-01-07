8d105673174c05206eb6c91895860ae83eec53eb
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-07 19:22:39 -0300

    ultimo ulyio

--- src/api/task.js
+++ src/api/task.js
@@ -1,12 +1,12 @@
 import axios from './axios.js'
 
-export const getTasksRequest = async () => axios.get('/tasks')
+export const getTasksRequest = async () => axios.get('api/tasks')
 
-export const getTaskRequest = async (id) => axios.get(`/tasks/${id}`)
+export const getTaskRequest = async (id) => axios.get(`api/tasks/${id}`)
 
-export const createTaskRequest = async (task) => axios.post('/tasks', task)
+export const createTaskRequest = async (task) => axios.post('api/tasks', task)
 
-export const updateTaskRequest = async (id, task) => axios.put(`/tasks/${id}`, task)
+export const updateTaskRequest = async (id, task) => axios.put(`api/tasks/${id}`, task)
 
-export const deleteTaskRequest = async (id) => axios.delete(`/tasks/${id}`)
+export const deleteTaskRequest = async (id) => axios.delete(`api/tasks/${id}`)
 
