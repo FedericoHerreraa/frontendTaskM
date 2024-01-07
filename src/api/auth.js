57bea7a411f8f0e49443cdd10cfda7d8b633c193
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-07 19:24:20 -0300

    ,

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,8 @@
 import axios from './axios.js'
 
-export const registerRequest = async (user) => axios.post('api/register', user)
+export const registerRequest = async (user) => axios.post('/register', user)
 
-export const loginRequest = async (user) => axios.post('api/login', user)
+export const loginRequest = async (user) => axios.post('/login', user)
 
-export const verifyTokenRequest = async () => axios.get('api/verify')
+export const verifyTokenRequest = async () => axios.get('/verify')
 
