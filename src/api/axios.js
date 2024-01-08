38030debedfd2b764e6ec3c4ca52682f933499c0
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-08 20:02:04 -0300

    ..

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://backendauth-vqbq.onrender.com'
+    baseURL: 'https://backendauth-vqbq.onrender.com/api'
 })
 
 export default instance
